afd39329ed7230dbedcc463b2ac799b595912258
appleair <applestven@icloud.com>
2025-07-07 22:22:06 +0800

    feat(index): 优化首页布局并添加完整的底部栏
    - 调整首页内容的排版方式，使其更加合理
    - 添加完整的底部栏，包括联系我们、产品功能、客户端下载和协议规则等信息
    - 更新底部栏样式，增加 logo 和公司介绍
    - 优化底部栏链接样式，使其更加清晰易读

--- pages/index.vue
+++ pages/index.vue
@@ -104,10 +104,12 @@
           <h1 class="text-[#00BC53] text-[36px] font-medium" data-aos="fade-up">ChatX 服务于全球的跨国创业者们，助您精准表达，无缝沟通。</h1>
           <p class="text-[16px] mt-[16px]" data-aos="fade-up">无论您对于ChatX的产品定价、方案定制或其他任何方面的问题 都可以随时联系我们</p>
         </div>
-        <div class="flex-1 gap-[16px] relative mr-[20px]" data-aos="fade-up"> 
+        <div class="flex-1 gap-[16px] relative mr-[20px]" data-aos="fade-up">
           <div class="flex items-center justify-center absolute right-0 bottom-[48px]">
-            <img data-aos="zoom-in-left" src="/ipx/index-btn-icon-2.webp" alt="ChatX出海翻译器,ChatX-banner" class="w-[48px] h-[48px] mt-[32px] mr-[24px] " />
-            <button class="cx-btn w-[240px] h-[48px] min-h-[64px] mt-[32px]" data-aos="fade-up" title="下载客户端" id="indexDownload">
+            <img data-aos="zoom-in-left" src="/ipx/index-btn-icon-2.webp" alt="ChatX出海翻译器,ChatX-banner"
+              class="w-[48px] h-[48px] mt-[32px] mr-[24px] " />
+            <button class="cx-btn w-[240px] h-[48px] min-h-[64px] mt-[32px]" data-aos="fade-up" title="下载客户端"
+              id="indexDownload">
               开始使用
             </button>
           </div>
@@ -115,18 +117,64 @@
       </div>
 
       <!-- 联系我们 -->
-      <div class="cx-footer-bar pt-[120px]">
-        <footer>
-          <div class="">
+      <div class="cx-footer-bar mt-[120px] pt-[68px]">
+        <footer class="flex justify-between w-[1200px]">
+          <div class="mb-[16px]">
             <router-link to="/" class="text-[#00BC53] text-[16px] font-medium">
               <img src="/ipx/logo2.webp" alt="ChatX Logo" class="w-[72px] h-[72px] inline-block mr-[8px]" />
             </router-link>
-            <router-link to="/" class="text-[#00BC53] text-[16px] font-medium">
+            <router-link to="/" class="text-[#00BC53] text-[16px] font-medium mb-[16px]">
               <img src="/ipx/logo3.webp" alt="ChatX Logo" class="w-[126px] ] inline-block mr-[8px]" />
             </router-link>
+            <p class="mt-[16px] text-[14px]"> ChatX 服务于全球的跨国创业者们，助您精准表达，无缝沟通。</p>
+          </div>
+
+          <div class="text-left">
+            <h3 data-v-a745acbc="">联系我们</h3>
+            <p data-v-a745acbc="">客服</p>
+            <a target="_blank" data-v-a745acbc="" href="https://t.me/ChatX788">@ChatX788</a>
+            <p data-v-a745acbc="">官方频道</p><a target="_blank" data-v-a745acbc=""
+              href="https://t.me/chatx_news">@chatx_news</a>
+            <p data-v-a745acbc="">联系邮箱</p><a href="mailto:chatx@gochatx.com" data-v-a745acbc="">chatx@gochatx.com</a>
+          </div>
+
+          <div class="text-left">
+            <h3 data-v-a745acbc="">产品功能</h3>
+            <p data-v-a745acbc=""><a href="/productTranslate" class="" id="footerProductTranslate"
+                data-v-a745acbc="">实时翻译 ·
+                智能多语⾔识别</a></p>
+            <p data-v-a745acbc=""><a href="/productPlatform" class="" id="footerProductPlatform"
+                data-v-a745acbc="">多平台多账号聚合管理</a></p>
+            <p data-v-a745acbc=""><a href="/productCallback" class="" id="footerProductCallback" data-v-a745acbc="">快捷回复
+                ·
+                ⼀键触达</a></p>
+            <p data-v-a745acbc=""><a href="/productAccount" class="" id="footerProductAccount" data-v-a745acbc="">智能指纹保护
+                安全护航</a></p>
+          </div>
+
+          <div class="text-left">
+            <h3 data-v-a745acbc="">客户端下载</h3>
+            <p data-v-a745acbc=""><a href="/download" class="" id="footerWindowDownload"
+                data-v-a745acbc="">Windows版本</a></p>
+            <p data-v-a745acbc=""><a href="/download" class="" id="footerMacDownload" data-v-a745acbc="">Mac版本</a></p>
+          </div>
+
+          <div class="text-left">
+            <h3 data-v-a745acbc="">协议规则</h3>
+            <p data-v-a745acbc=""><a href="/serviceAgreement" class="" id="serviceAgreement" data-v-a745acbc="">服务协议</a>
+            </p>
+            <p data-v-a745acbc=""><a href="/privacyPolicy" class="" id="privacyPolicy" data-v-a745acbc="">隐私规则</a></p>
+            <p data-v-a745acbc=""><a href="/disclaimers" class="" id="disclaimers" data-v-a745acbc="">免责声明</a></p>
+            <p data-v-a745acbc=""><a href="/letterCommitment" class="" id="letterCommitment" data-v-a745acbc="">承诺书</a>
+            </p>
           </div>
         </footer>
+        <div class="border-t-[1px] border-[#ebebeb] w-full h-[55px] pt-[24px]" >
+          <p> © 2021 - 2024 gochatx.com All Rights Reserved</p>
+        </div>
+        
       </div>
+      
 
     </div>
   </section>
